974223f1476913fd1a758b2d5ff67b38938a4606
王伟 <ww36642@ly.com>
2017-02-14 10:00:38 +0800

    init

---
+++ Utility/RequestHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Web
+{
+    /// <summary>
+    /// HttpRequest的拓展方法
+    /// </summary>
+    public static class RequestHelper
+    {
+        /// <summary>
+        /// 判断当前请求是否是POST请求
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static bool IsPostBack(this HttpRequest request)
+        {
+            return "post".Equals(request.HttpMethod, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        /// <summary>
+        /// 获取请求参数中的Int类型值
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="key"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static int GetInt(this HttpRequest request, string key, int def = 0)
+        {
+            int res;
+            var temp = request[key];
+            if (int.TryParse(temp, out res))
+            {
+                return res;
+            }
+            return def;
+        }
+    }
+}
---
+++ Utility/StreamExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace System.IO
+{
+    /// <summary>
+    /// 文件流的拓展方法
+    /// </summary>
+    public static class StreamExtension
+    {
+
+        #region 获取数据流的内容MD5
+
+        /// <summary>
+        /// 获取数据流的内容MD5
+        /// </summary>
+        /// <param name="stream">流</param>
+        /// <returns>MD5</returns>
+        public static string GetMd5(this Stream stream)
+        {
+            var oMd5Hasher = new MD5CryptoServiceProvider();
+            var arrbytHashValue = oMd5Hasher.ComputeHash(stream);
+
+            #region 重构前
+
+            //// 由以连字符分隔的十六进制对构成的String，其中每一对表示value 中对应的元素；例如“F-2C-4A”
+            //string strHashData = BitConverter.ToString(arrbytHashValue);
+
+            //// 替换-
+            //return strHashData.Replace("-", string.Empty).ToLower();
+
+            #endregion
+
+            var sbd = new StringBuilder();
+            foreach (var item in arrbytHashValue)
+            {
+                sbd.Append(item.ToString("x2"));
+            }
+            return sbd.ToString();
+
+        }
+
+        #endregion
+
+    }
+}
